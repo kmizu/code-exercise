281935de74de479662a4c94b444ad287e1978e11
Kota Mizushima <mizukota@gmail.com>
2013-10-08 21:29:45 +0900

    Add `solveFunctional` to solve this problem more functional way.

--- src/main/scala/com/github/kmizu/code_exercise/problems/M1_6.scala
+++ src/main/scala/com/github/kmizu/code_exercise/problems/M1_6.scala
@@ -13,6 +13,17 @@ object M1_6 {
     answer
   }
 
+  def solveFunctional(a: Int*): Int = {
+    val n = a.length
+    val seeds = for (i <- 0 until n; j <- (i + 1) until n; k <- (j + 1) until n) yield {
+      val len = a(i) + a(j) + a(k)
+      val mx = a(i) max a(j) max a(k)
+      (len, mx)
+    }
+    seeds.foldLeft(0){
+      case (ans, (len, mx)) => if(mx * 2 < len) ans max len else ans
+    }
+  }
 
 
 }
--- src/test/scala/com/github/kmizu/code_exercise/problems/M1_6UnitSpec.scala
+++ src/test/scala/com/github/kmizu/code_exercise/problems/M1_6UnitSpec.scala
@@ -6,10 +6,15 @@ import M1_6._
 class M1_6UnitSpec extends FlatSpec {
 
   "M1_6" should "have answer in some inputs" in {
-    assert(solveNaive(2, 3, 4, 5, 10) === 12)
+    assertResult(12)(solveNaive(2, 3, 4, 5, 10))
   }
 
   it should "not have answer otherwise" in {
-    assert(solveNaive(4, 5, 10, 20) === 0)
+    assertResult(0)(solveNaive(4, 5, 10, 20))
+  }
+
+  "solveFunctional" should "returns the same value with solveNaive" in {
+    assertResult(solveNaive(2, 3, 4, 5, 10))(solveFunctional(2, 3, 4, 5, 10))
+    assertResult(solveNaive(4, 5, 10, 20))(solveFunctional(4, 5, 10, 20))
   }
 }
