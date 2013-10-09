30b3d3745c821281693784f718e70d17cfa089f8
Kota Mizushima <mizukota@gmail.com>
2013-10-09 09:22:33 +0900

    Write more tests about 'solveEfficiently'

--- src/test/scala/com/github/kmizu/code_exercise/problems/M1_6UnitSpec.scala
+++ src/test/scala/com/github/kmizu/code_exercise/problems/M1_6UnitSpec.scala
@@ -7,10 +7,12 @@ class M1_6UnitSpec extends FlatSpec {
 
   "M1_6" should "have answer in some inputs" in {
     assertResult(12)(solveNaive(2, 3, 4, 5, 10))
+    assertResult(29)(solveNaive(2, 3, 4, 5, 10, 14))
   }
 
   it should "not have answer otherwise" in {
     assertResult(0)(solveNaive(4, 5, 10, 20))
+    assertResult(0)(solveNaive(0, 1, 15, 25))
   }
 
   "solveFunctional" should "returns the same value with solveNaive" in {
@@ -18,8 +20,18 @@ class M1_6UnitSpec extends FlatSpec {
     assertResult(solveNaive(4, 5, 10, 20))(solveFunctional(4, 5, 10, 20))
   }
 
+  "solveFunctional" should "returns the same value with solveNaive in the case there are no answer" in {
+    assertResult(solveNaive(4, 5, 10, 20))(solveFunctional(4, 5, 10, 20))
+    assertResult(solveNaive(0, 1, 15, 25))(solveFunctional(0, 1, 15, 25))
+  }
+
   "solveEfficiently" should "returns the same value with solveNaive" in {
     assertResult(solveNaive(4, 5, 10, 20))(solveEfficiently(4, 5, 10, 20))
+    assertResult(solveNaive(2, 3, 4, 5, 10, 14))(solveEfficiently(2, 3, 4, 5, 10, 14))
+  }
+
+  "solveEfficiently" should "returns the same value with solveNaive in the case there are no answer" in {
     assertResult(solveNaive(4, 5, 10, 20))(solveEfficiently(4, 5, 10, 20))
+    assertResult(solveNaive(0, 1, 15, 25))(solveEfficiently(0, 1, 15, 25))
   }
 }
