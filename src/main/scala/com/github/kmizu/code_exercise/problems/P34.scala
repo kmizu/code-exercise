6a11d5ee996c4e5adebce747e1fb80af82eda81f
Kota Mizushima <mizukota@gmail.com>
2013-10-18 00:24:03 +0900

    P33 has been solved in more effectively

--- src/main/scala/com/github/kmizu/code_exercise/problems/P34.scala
+++ src/main/scala/com/github/kmizu/code_exercise/problems/P34.scala
@@ -1,5 +1,8 @@
 package com.github.kmizu.code_exercise.problems
 
+import scala.util.control.Breaks._
+import scala.collection.mutable
+
 class P34 {
   def solveNaive(k: Int, as: Seq[Int]): Boolean = {
     (1 to as.size).exists{i =>
@@ -8,7 +11,24 @@ class P34 {
   }
 
   def solve(k: Int, as: Seq[Int]): Boolean = {
-    val bs = as.filter{_ <= k}.sorted
-    ???
+    val nums = as.sorted.toArray
+
+    val ans = Array.ofDim[Int](k + 1)
+    ans(0) = 0
+    (1 to k).foreach{i => ans(i) = -1}
+    breakable {
+      for (v <- nums) {
+        for (j <- Range(k, -1, -1) if ans(j) != -1) {
+          val x = j + v
+          if (x <= k && ans(x) == -1) ans(x) = v
+        }
+        if (ans(k) != -1) {
+          break()
+        }
+
+      }
+    }
+
+    ans(k) != -1
   }
 }
--- src/test/scala/com/github/kmizu/code_exercise/problems/P34UnitSpec.scala
+++ src/test/scala/com/github/kmizu/code_exercise/problems/P34UnitSpec.scala
@@ -8,5 +8,11 @@ class P34UnitSpec extends FlatSpec {
 
   "P34#solveNaive" should "return true for the following input" in {
     assertResult(true)(solveNaive(13, Seq(1, 2, 4, 7)))
+    assertResult(true)(solveNaive(9, Seq(1, 2, 4, 7)))
+  }
+
+  "P34#solve" should "return true for the following input" in {
+    assertResult(true)(solve(13, Seq(1, 2, 4, 7)))
+    assertResult(true)(solve(9, Seq(1, 2, 4, 7)))
   }
 }
