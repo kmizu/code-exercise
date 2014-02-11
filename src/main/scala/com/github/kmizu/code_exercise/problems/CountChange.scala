fe28bf1e9538194a85a0c82660e559bcc37f4c66
Kota Mizushima <mizukota@gmail.com>
2014-02-12 00:10:54 +0900

    Add primary constructor taking one Int argument

--- src/main/scala/com/github/kmizu/code_exercise/problems/CountChange.scala
+++ src/main/scala/com/github/kmizu/code_exercise/problems/CountChange.scala
@@ -4,12 +4,14 @@ package com.github.kmizu.code_exercise.problems
  * @author Kota Mizushima
  */
 class CountChange {
-  sealed abstract class Coin
-  case object C001 extends Coin
-  case object C005 extends Coin
-  case object C010 extends Coin
-  case object C050 extends Coin
-  case object C100 extends Coin
-  case object C500 extends Coin
-  def solve(amount: Int, contraints: Map[Coin, Int]): (Int, Map[Coin, Int]) = ???
+  sealed abstract class Coin(val value: Int)
+  case object C001 extends Coin(1)
+  case object C005 extends Coin(5)
+  case object C010 extends Coin(10)
+  case object C050 extends Coin(50)
+  case object C100 extends Coin(100)
+  case object C500 extends Coin(500)
+
+  def solve(amount: Int, constraints: Map[Coin, Int]): (Int, Map[Coin, Int]) = {
+  }
 }
