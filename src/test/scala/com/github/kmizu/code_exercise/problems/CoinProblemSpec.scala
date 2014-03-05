810aef643a532a1eb0b303e806801cd08aeacaa7
Kota Mizushima <mizukota@gmail.com>
2014-03-06 00:29:26 +0900

    Ranaming (2).

--- src/test/scala/com/github/kmizu/code_exercise/problems/CoinProblemSpec.scala
+++ src/test/scala/com/github/kmizu/code_exercise/problems/CoinProblemSpec.scala
@@ -6,11 +6,11 @@ class CoinProblemSpec extends FlatSpec {
   val S = new CoinProblem
   import S._
 
-  "CountChange#solve" should "return the following result" in {
+  "CoinProblem#solve" should "return the following result" in {
     assertResult(Some(List(C500 -> 1, C050 -> 2, C010 -> 1, C005 -> 2)))(solveNaive(620, List(C001 -> 3, C005 -> 2, C010 -> 1, C050 -> 3, C100 -> 0, C500 -> 2)))
   }
 
-  "CountChange#solveByDP" should "return the following result" in {
+  "CoinProblem#solveByDP" should "return the following result" in {
     assertResult(Some(List(C500 -> 2, C100 -> 2, C010 -> 2)))(solveByDP(1220, List(C001 -> 5, C005 -> 0, C010 -> 5, C050 -> 0, C100 -> 5, C500 -> 3)))
   }
 }
